99d0f3ed40d1cc0d4c6db2ce1c46f3f4f75ece5f
mafusailovich <vfesenko@yandex.ru>
2022-12-24 00:21:08 +0300

    D10
    Выполнено задание модуля D10

--- NewsPaper/NewsPaper/celery.py
+++ NewsPaper/NewsPaper/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
@@ -8,3 +9,11 @@ app = Celery('NewsPaper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+        'week_news_every_monday_8am': {
+        'task': 'news.tasks.week_news',
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+    },
+}
+app.conf.timezone = 'Europe/Moscow'
--- NewsPaper/news/tasks.py
+++ NewsPaper/news/tasks.py
@@ -1,17 +1,12 @@
 from celery import shared_task
 import time
+from datetime import datetime,timedelta
 
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
-from .models import Post, Category
+from .models import Post, Category, User
 
 
-
-@shared_task
-def hello():
-    for i in range(10):
-        time.sleep(10)
-        print("Hello, world!")
 
 @shared_task
 def notification(post_id, post_head, post_text):
@@ -32,6 +27,37 @@ def notification(post_id, post_head, post_text):
             body=f'Здравствуйте, {subscriber.username}. Появилась новая статья в вашем любимом разделе.',
             from_email='test1@sch1935.site',
             to=[subscriber.email],
+        )
+        msg.attach_alternative(html_content, "text/html")
+
+        msg.send()
+
+
+@shared_task
+def week_news():
+    last_week_date = (datetime.now()-timedelta(days=7)).date() #дата неделей раньше
+    post_week_list = Post.objects.filter(time_in__gt=last_week_date) #статьи за прошлую неделю
+    user_list = set(User.objects.filter(category__isnull=False)) #только пользователи, которые подписаны
+    #так как после данной фильтрации получается список с кучей одинаковых записей, то избавляюсь при помощи множества
+    for user in user_list:
+        user_c = user.category_set.all().values()
+        user_c = [i['id'] for i in user_c]
+
+        post_list_for_user = []
+        for p in post_week_list:
+            for j in p.category.values():
+                if j['id'] in user_c:
+                    post_list_for_user.append(p)
+                    break
+
+        html_content = render_to_string('email_create_r.html',
+                                        {'username': user.username, 'posts': post_list_for_user})
+
+        msg = EmailMultiAlternatives(
+            subject=f'Список статей за неделю',
+            body=f'Здравствуйте, {user.username}. Появилась новая статья в вашем любимом разделе.',
+            from_email='test1@sch1935.site',
+            to=[user.email],
         )
         msg.attach_alternative(html_content, "text/html")
 
